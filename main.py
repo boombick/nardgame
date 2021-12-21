4b3d16a60217c6cdd44962af4e826b2ce101a017
Andrey Sinitsyn <andrey.sinitsin@masterdelivery.ru>
2021-12-21 14:44:28 +0300

    Added web client template

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World not a world"}
+        return {"Message": "medium"}
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
